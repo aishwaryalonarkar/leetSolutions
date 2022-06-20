a93aca09eb256700ef2217117c95e0658a717ca9
aishwaryalonarkar <aishwaryalonarkar@gmail.com>
2022-06-20 13:42:48 -0400

    fibdp

---
+++ fibdp.py
@@ -0,0 +1,19 @@
+class Fibonacci :
+    def __init__(self) :
+        self.dp = []
+
+    def fib(self,n) :
+        for i in range(0,n+1) :
+            self.dp.append(-1)
+
+        self.dp[0] = 0
+        self.dp[1] = 1
+
+        for i in range(2,n+1):
+            self.dp[i] = self.dp[i-2] + self.dp[i-1]
+            # print(self.dp[i])
+        
+        return self.dp[n]
+
+f = Fibonacci()
+print(f.fib(5))
