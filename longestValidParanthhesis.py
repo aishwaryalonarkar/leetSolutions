870f51ad4fe25a434deb0f2cf08ba2eb872e6e5a
aishwaryalonarkar <aishwaryalonarkar@gmail.com>
2022-06-23 04:35:44 -0400

    longest Valid Paranthesis Count

---
+++ longestValidParanthhesis.py
@@ -0,0 +1,36 @@
+
+def getLongest(s) :
+    result = ""
+    counter = 0
+    stack = []
+
+    r=0
+    l=0
+    i=0
+    maxLen = 0
+
+    while i<len(s):
+        if s[i] == "(":
+            l+=1
+        else :
+            r+=1
+        if r==l :
+            maxLen = max(maxLen, l+r)
+        elif r>= l :
+            l=r=0
+        i+=1
+
+    i-=1
+    while i>0:
+        if s[i] == "(":
+            l+=1
+        else :
+            r+=1
+        if r==l :
+            maxLen = max(maxLen, l+r)
+        elif r<= l :
+            l=r=0
+        i-=1
+    return maxLen
+
+print(getLongest("))))()()))"))
